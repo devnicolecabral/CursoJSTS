8f8c9602b9985afa610d680f3eeadce4eb8f0f70
Nicole Cabral <nscnick22@gmail.com>
2026-02-12 14:29:55 -0300

    Aulas 8 e 9

---
+++ aula 8/index.js
@@ -0,0 +1,26 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84kg 
+tem 1.8 de altura e seu IMC é de 25.925925925925924.
+Luiz Otávio nasceu em 1980. 
+*/
+
+const nome = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84; 
+const alturaEmM = 1.80; 
+let imc; // peso / (altura * altura)
+let anoNascimento;
+imc = peso / (alturaEmM * alturaEmM);
+anoNascimento = 2026 - idade;
+
+/* console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso, 'kg.');
+console.log('tem', alturaEmM, 'e seu íncice de massa corporal é de', imc);
+console.log(nome, 'nasceu em', anoNascimento);
+*/
+
+//template strings = une com crase 
+console.log(`${nome} ${sobrenome} tem ${idade} anos pesa ${peso} kg.`);
+console.log(`tem ${alturaEmM} e seu íncice de massa corporal é de ${imc}`);
+console.log(`${nome} nasceu em ${anoNascimento}`);
+
---
+++ aula 9/index.js
@@ -0,0 +1,8 @@
+//ECMAScript 2015 (ES6)
+
+var nome = 'Luiz';
+var nome = 'Otávio';
+
+console.log(nome);
+
+//pode declarar e redeclarar com VAR e com LET não pode 
