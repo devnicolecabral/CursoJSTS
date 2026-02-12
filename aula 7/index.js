939156e1255e645a75bb32914e59715bdae15f59
Nicole Cabral <nscnick22@gmail.com>
2026-02-11 22:35:32 -0300

    aula 7

---
+++ aula 7/index.js
@@ -0,0 +1,16 @@
+// Não podemos criar constantes com palavras reservadas 
+// Constantes PRECISAM ter nomes significativos
+// Não pode começar o nome de uma constante com um número 
+// Não podem conter espaços ou traços. Utilizamos camelCase em javascript 
+// Case-sensitive (letras maiusculas e minusculas fazem diferença)
+// Não pode modificar o valor de uma constante
+// A constante ja precisa ser criada e inicializada ao mesmo tempo
+
+const primeiroNumero = 5;
+const segundoNumero = 10; 
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadTriplicado =  resultado * 3;
+let resultadoTriplicado =  resultadTriplicado + 5;
+console.log(typeof (primeiroNumero + segundoNumero));
+
