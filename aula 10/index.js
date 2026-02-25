eba214d2faa1b529b5bddc3950222d79e115579b
Nicole Cabral <nscnick22@gmail.com>
2026-02-25 10:35:55 -0300

    aula 10
    tipos de dados primitivos

---
+++ aula 10/index.js
@@ -0,0 +1,12 @@
+const nome = 'Luiz';  // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+
+const num1 = 10; // number 
+const num2 = 10.52; // number 
+
+let nomeAluno; // undefined = não aponta para local nenhum na memória 
+const sobrenomeAluno = null; //esse tipo de valor significa nulo, que também não aponta para local algum na memória. O null utiliza quando queremos desconfigurar uma variável. 
+
+const aprovado = true; // Boolean = true, false (valores logicos)
+
