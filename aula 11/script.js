5d09d793c310d06a438996fe005c4d93c8c34817
Nicole Cabral <nscnick22@gmail.com>
2026-03-02 13:26:22 -0300

    Exercício com variáveis

--- aula 11/script.js
+++ aula 11/script.js
@@ -1,2 +1,10 @@
-alert('Com a nossa mensagem');
 //Estudo no navegador alert, confirm e prompt
+
+let num1 = prompt('Digite um número: ');
+let num2 = prompt('Digite outro número: ');
+
+num1 = Number(num1)
+num2 = Number(num2)
+
+const resultado = num1 + num2;
+alert(`O resultado da sua conta foi: ${resultado}`);
